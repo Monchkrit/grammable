598b513b74987e6ec37b0136395512f6628893ab
Monchkrit <blitzkritter@gmail.com>
2019-03-30 13:59:51 +0000

    The gram update tests are passing.

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,20 +1,18 @@
 class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
+  def new
+    @gram = Gram.new
+  end
+
   def index
   end
 
   def show
     @gram = Gram.find_by_id(params[:id])
-    if @gram.blank?
-      render plain: 'Not Found :(', status: :not_found
-    end
+    return render_not_found if @gram.blank?
   end
-
-  def new
-    @gram = Gram.new
-  end
-
+  
   def create
     @gram = current_user.grams.create(gram_params)
     if @gram.valid?
@@ -24,9 +22,29 @@ class GramsController < ApplicationController
     end
   end
 
+  def edit
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+  end
+
+  def update
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    @gram.update_attributes(gram_params)
+    if @gram.valid?
+      redirect_to root_path
+    else
+      return render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def gram_params
     params.require(:gram).permit(:message)
+  end
+
+  def render_not_found
+    render plain: 'Not Found :(', status: :not_found
   end
 end
